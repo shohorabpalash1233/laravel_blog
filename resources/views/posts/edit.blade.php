3c33d16f7ded4b20af7c2fc206f073fe6d8fec2d
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-11-09 18:54:14 +0600

    post edit page updated

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,10 +6,19 @@
 
 <div class="row">
 
-	<div class="col-md-8">
-		<h1>{{ $post->title }}</h1>
+{!! Form::model($post, ['route' => ['posts.update', $post->id], 'method' => 'PUT' ]) !!}
 
-		<p class="lead">{{ $post->body }}</p>
+	<div class="col-md-8">
+
+		{{ Form::label('title', 'Title: ') }}
+		{{ Form::text('title', null, ['class' => 'form-control input-lg ']) }}
+
+		{{ Form::label('slug', 'Slug: ', ['class' => 'form-spacing-top']) }}
+		{{ Form::text('slug', null, ['class' => 'form-control input']) }}
+
+		{{ Form::label('body', 'Body: ', ['class' => 'form-spacing-top']) }}
+		{{ Form::textarea('body', null, ['class' => 'form-control']) }}
+
 	</div>
 
 	<div class="col-md-4">
@@ -28,15 +37,17 @@
 
 			<div class="row">
 				<div class="col-sm-6">
-					{!! Html::linkRoute('posts.edit', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block') ) !!}
+					{!! Html::linkRoute('posts.show', 'Cancel', array($post->id), array('class' => 'btn btn-danger btn-block') ) !!}
 				</div>
 
 				<div class="col-sm-6">
-					{!! Html::linkRoute('posts.update', 'Save', array($post->id), array('class' => 'btn btn-success btn-block') ) !!}
+					{{ Form::submit('Save Changes', ['class' => 'btn btn-success btn-block']) }}
 				</div>
 			</div>
 		</div>
 	</div>
+
+{!! Form::close() !!}
 
 </div>
 
