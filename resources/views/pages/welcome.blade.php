cb2e91b6d09fb009c76c864c5f9367be5fb1bfed
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-11-09 18:52:11 +0600

    added slugs to the link

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -15,53 +15,26 @@
 </div> 
 <div class="row">
     <div class="col-md-8">
+        
+        @foreach($posts as $post)
+
         <div class="post">
-            <h3>Post Title</h3>
-            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
-            <a href="" class="btn btn-primary" title="">Read More</a>
+            <h3>{{ $post->title }}</h3>
+            <p align="justify">{{ substr($post->body, 0, 300)}} {{ strlen($post->body) > 300 ? "..." : "" }}</p>
+            <a href="{{ url('blog/'.$post->slug) }}" class="btn btn-primary" title="">Read More</a>
         </div>
+
         <hr>
-        <div class="post">
-            <h3>Post Title</h3>
-            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
-            <a href="" class="btn btn-primary" title="">Read More</a>
-        </div>
-        <hr>
-        <div class="post">
-            <h3>Post Title</h3>
-            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
-            <a href="" class="btn btn-primary" title="">Read More</a>
-        </div>
-        <hr>
-        <div class="post">
-            <h3>Post Title</h3>
-            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</p>
-            <a href="" class="btn btn-primary" title="">Read More</a>
-        </div>
+
+        @endforeach
+
     </div>
     <div class="col-md-3 col-md-offset-1">
         <h2>Sidebar</h2>
     </div>
 </div>
+            <div class="text-center">
+                {!! $posts->links() !!}
+            </div>
 
 @endsection
